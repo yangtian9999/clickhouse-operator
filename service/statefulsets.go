8f9ffc9d3d995cfcb7cfc801c0fbc0550eb9568f
yangtian9999 <yangtian9999@163.com>
2022-09-19 15:17:48 +0800

    fix statefulset and pvc

--- service/statefulsets.go
+++ service/statefulsets.go
@@ -6,6 +6,7 @@ import (
 	"github.com/yangtian9999/clickhouse-operator/api/v1alpha1"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
@@ -49,38 +50,30 @@ func NewStatefulsSet(db *v1alpha1.Ckinstance, scheme *runtime.Scheme) *appsv1.St
 							Name:          "tcpport",
 						}},
 						VolumeMounts: []corev1.VolumeMount{{
-							Name:      "clickhouse-data",
+							Name:      db.Spec.DatabaseName + "-data",
 							MountPath: "/etc/clickhouse-server/config.d/",
 							// MountPath: "/etc/clickhouse-server/",
 						}},
 					}},
-					// Volumes: []corev1.Volume{{
-					// 	Name: "click",
-					// 	VolumeSource: corev1.VolumeSource{
-					// 		PersistentVolumeClaim: &corev1.PersistentVolumeClaimVolumeSource{
-					// 			ClaimName: "click",
-					// 		},
-					// 	},
-					// }},
 				},
 			},
-			VolumeClaimTemplates: []corev1.PersistentVolumeClaim{
-				// 	ObjectMeta: metav1.ObjectMeta{
-				// 		Name:      "clickhouse-data",
-				// 		Namespace: db.Spec.Namespace,
-				// 	},
-				// 	Spec: corev1.PersistentVolumeClaimSpec{
-				// 		AccessModes: []corev1.PersistentVolumeAccessMode{
-				// 			corev1.ReadWriteOnce,
-				// 		},
-				// 		Resources: corev1.ResourceRequirements{
-				// 			Requests: corev1.ResourceList{
-				// 				corev1.ResourceStorage: resource.MustParse(db.Spec.DatabaseStorageRequest),
-				// 			},
-				// 		},
-				// 	},
-				// 	StorageClassName: &db.Spec.DatabaseStorageClassName,
-			},
+			VolumeClaimTemplates: []corev1.PersistentVolumeClaim{{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      db.Spec.DatabaseName + "-data",
+					Namespace: db.Spec.Namespace,
+				},
+				Spec: corev1.PersistentVolumeClaimSpec{
+					AccessModes: []corev1.PersistentVolumeAccessMode{
+						corev1.ReadWriteOnce,
+					},
+					Resources: corev1.ResourceRequirements{
+						Requests: corev1.ResourceList{
+							corev1.ResourceStorage: resource.MustParse(db.Spec.DatabaseStorageRequest),
+						},
+					},
+					StorageClassName: &db.Spec.DatabaseStorageClassName,
+				},
+			}},
 		},
 	}
 	controllerutil.SetControllerReference(db, sta, scheme)
